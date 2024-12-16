571fc9314116cc61b75c0b6630ff714fd08e7a43
unknown <fabioflauzino@hotmail.com>
2024-12-15 23:28:12 -0300

    Criando template

---
+++ src/pages/essenciais/contador.tsx
@@ -0,0 +1,10 @@
+import Pagina from "@/components/template/Pagina";
+
+export default function () {
+  return (
+    <Pagina 
+    titulo="Contador"
+    subtitulo="Entendendo o que é um estado."
+    ></Pagina>
+  )
+}
