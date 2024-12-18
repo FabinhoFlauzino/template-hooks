3d768edbd301098b42b6870e171e628cb66c92ef
Fabio Flauzino <fabio@mfrural.com.br>
2024-12-18 09:17:43 -0300

    Aplicando o contextoTema aos Componentes

--- src/components/template/SeletorDeCor.tsx
+++ src/components/template/SeletorDeCor.tsx
@@ -3,9 +3,15 @@ import Botao from "./Botao"
 import Flex from "./Flex"
 
 export default function SeletorDeCor() {
-  let {cores} = useTema()
+  let { cores, setCorDestaque } = useTema()
 
-  let listaCores = cores.map((cor: string) => <Botao key={cor} cor={`bg-${cor}`} />)
+  let listaCores = cores.map((cor: string) => (
+    <Botao
+      key={cor}
+      cor={`bg-${cor}`}
+      onClick={() => { setCorDestaque(cor) }}
+    />
+  ))
 
   return (
     <Flex center gap={4} className="absolute right-3 top-20">
