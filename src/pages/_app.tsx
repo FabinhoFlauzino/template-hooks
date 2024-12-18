1c2de781eb38a2a2f5dbd274e2db0418ba68e47a
Fabio Flauzino <fabio@mfrural.com.br>
2024-12-18 10:58:19 -0300

    Aplicando o contextoMenu aos Componentes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { TemaProvider } from "@/data/context/TemaProvider";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { MenuProvider } from "@/data/context/MenuProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <TemaProvider>
-            <Component {...pageProps} />
+            <MenuProvider>
+                <Component {...pageProps} />
+            </MenuProvider>
         </TemaProvider>
     );
 }
